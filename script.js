09edfd84b0de07b2da9155a14e507257cd0cd4b9
jku784805-debug <jku784805@gmail.com>
2026-01-27 14:03:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -210,12 +210,3 @@ document.addEventListener('click', (e) => {
     if(viewer) { viewer.style.display = 'none'; document.body.style.overflow = ''; }
   }
 });
-// MENU MOBILE
-const burgerBtn = document.getElementById("burgerBtn");
-const mobileMenu = document.getElementById("mobileMenu");
-
-burgerBtn.addEventListener("click", () => {
-  mobileMenu.style.display =
-    mobileMenu.style.display === "flex" ? "none" : "flex";
-});
-
